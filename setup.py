397f2c7cda223d5d5e5214b760f2275dcd841a17
Keshav11081996 <maheshwarikeshav252@gmail.com>
2024-11-21 01:08:00 +0530

    Initial commit with GitHub Actions workflow

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mypackage",
+    version="0.1",
+    description="A simple example package",
+    author="Keshav",
+    packages=find_packages(),
+    install_requires=[],
+)
